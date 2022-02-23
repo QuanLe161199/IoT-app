9bcc3c053c4bfb0c9781e70436a9e5b89765fac0
Quan Le <quanlv161199@gmail.com>
2022-02-23 16:34:32 +0700

    edit text form

--- screens/DeviceManager.js
+++ screens/DeviceManager.js
@@ -659,7 +659,7 @@ const styles = StyleSheet.create({
         marginBottom: 10,
     },
     sensorText: {
-        fontSize: 25,
+        fontSize: 18,
         marginTop: 30,
         color: "white",
     },
